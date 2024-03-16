fbb56f5828c41a2559cac8efc9629ddf805a5541
Zoom2432 <vsgz2432@gmail.com>
2024-03-16 22:10:21 +0300

    created fraction class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction n1 = new Fraction();
+        Fraction n2 = new Fraction(5);
+        Fraction n3 = new Fraction(3, 4);
+
+        n1.SetBottom(2);
+        Console.WriteLine(n1.GetBottom());
+
+        Console.WriteLine(n1.GetFractionString());
+        Console.WriteLine(n1.GetDecimalValue());
+
+        Console.WriteLine(n2.GetFractionString());
+        Console.WriteLine(n2.GetDecimalValue());
+
+        Console.WriteLine(n3.GetFractionString());
+        Console.WriteLine(n3.GetDecimalValue());
     }
 }
