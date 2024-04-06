22ff52d2270e366027642a9261c3085edf3cc32c
Zoom2432 <vsgz2432@gmail.com>
2024-04-06 22:40:16 +0300

    completed Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square sq = new Square(4, "red");
+        Rectangle rec = new Rectangle(3, 5, "blue");
+        Circle cr = new Circle(10, "black");
+
+        List <Shape> figures = new List<Shape>();
+
+        figures.Add(sq);
+        figures.Add(rec);
+        figures.Add(cr);
+
+        foreach(Shape sh in figures)
+        {
+            Console.WriteLine(sh.GetArea());
+            Console.WriteLine(sh.GetColor());
+        }
     }
 }
